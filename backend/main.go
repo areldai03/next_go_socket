977b9bcc707fa3f4715a9df4b6dd5d4833d03aa6
areldai03 <dai.0127@icloud.com>
2024-03-08 15:58:48 +0900

    connect front to back

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,49 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/labstack/echo/v4"
-    "github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/net/websocket"
 )
+
+func handleWebSocket(c echo.Context) error {
+	log.Println("Serving at localhost:8000...")
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+
+		// 初回のメッセージを送信
+		err := websocket.Message.Send(ws, "Server: Hello, Next.js!")
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		for {
+			// Client からのメッセージを読み込む
+			msg := ""
+			err := websocket.Message.Receive(ws, &msg)
+			log.Println(msg)
+			if err != nil {
+				if err.Error() == "EOF" {
+					log.Println(fmt.Errorf("read %s", err))
+					break
+				}
+				log.Println(fmt.Errorf("read %s", err))
+				c.Logger().Error(err)
+			}
+
+			// Client からのメッセージを元に返すメッセージを作成し送信する
+			err = websocket.Message.Send(ws, fmt.Sprintf("Server: \"%s\" received!", msg))
+			if err != nil {
+				c.Logger().Error(err)
+			}
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
+}
 
 func main() {
     e := echo.New() // インスタンスを作成
@@ -14,6 +52,8 @@ func main() {
     e.GET("/", func(c echo.Context) error { // ルートを設定
 	    return c.String(http.StatusOK, "Hello, World!") // 出力
     })
+
+    e.GET("/socket", handleWebSocket)
 
     e.Logger.Fatal(e.Start(":8080")) // サーバーをポート番号8080で起動
 }
